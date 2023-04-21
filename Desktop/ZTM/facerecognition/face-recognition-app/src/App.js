c883b4a6131ce998e027ab8886a22d1ef1d70595
Aayushsoni09 <44709621+Aayushsoni09@users.noreply.github.com>
2023-04-21 12:41:22 +0530

    added the signin and register form

--- Desktop/ZTM/facerecognition/face-recognition-app/src/App.js
+++ Desktop/ZTM/facerecognition/face-recognition-app/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import React from 'react'
 import ParticlesBg from 'particles-bg'
@@ -5,14 +6,48 @@ import Navigation from  './components/navigation/Navigation'
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/image-link-form/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import Signin from './components/signin/Signin';
+import Register from './components/register/Register';
+
+
 function App() {
+  const [route,setRoute]=useState({
+    route :'signin',
+  })
+  const [signedIn,setSignedIn]=useState({
+    isSignedIn:false,
+  })
+
+  const onRouteChange=(route)=>{
+    if(route==='signout'){
+      setSignedIn({isSignedIn:false})
+    }
+    else if(route ==='home'){
+      setSignedIn({isSignedIn:true})
+    }
+    setRoute({route:route})
+  }
+
   return (
     <div className="App">
       <ParticlesBg color="#FEFDFD" type="cobweb" bg={true} />
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
+      <Navigation onRouteChange={onRouteChange} isSignedIn={signedIn}/>
+      {route.route === 'home' ?
+         <div>
+            <Logo/>
+            <Rank/>
+            <ImageLinkForm/>
+          </div>
+      :
+      (
+        route.route === 'signin' ?
+        <Signin onRouteChange={onRouteChange} isSignedIn={route}/>
+        :
+        <Register onRouteChange={onRouteChange}/>
+      )
+
+  }
+
     </div>
   );
 }
--- Desktop/ZTM/facerecognition/face-recognition-app/src/components/navigation/Navigation.jsx
+++ Desktop/ZTM/facerecognition/face-recognition-app/src/components/navigation/Navigation.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
-const Navigation = () => {
-  return (
-    <div style={{display:'flex' ,justifyContent:'flex-end'}}>
-       <p className='f3 link dim white underline pa3 pointer'>Signout</p> 
-    </div>
-  )
+const Navigation = ({onRouteChange,isSignedIn}) => {
+  console.log(isSignedIn.isSignedIn)
+  console.log('signout pe aaya')
+  if (isSignedIn.isSignedIn){
+    return (
+      <nav style={{display:'flex' ,justifyContent:'flex-end'}}>
+         <p onClick={()=>onRouteChange('signout')} className='f3 link dim white underline pa3 pointer'>Signout</p> 
+      </nav>)
+  }
+  else{
+    return (
+      <nav style={{display:'flex' ,justifyContent:'flex-end'}}>
+        <>
+        {console.log('signout ke else pe aaya')}
+        <p onClick={()=>onRouteChange('signin')} className='f3 link dim white underline pa3 pointer'>Signin</p> 
+        <p onClick={()=>onRouteChange('register')} className='f3 link dim white underline pa3 pointer'>Register</p> 
+        </>
+         
+      </nav>
+    )
+  }
+  
 }
 
 export default Navigation
